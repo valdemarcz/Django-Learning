7f87065191603068afd534006d277d6322260f95
czerniavski <v.czerniavski@gmail.com>
2021-02-07 16:08:18 +0200

    AMQPS service connection, consumer and producer added

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+# amqps://lfjntxwl:ajLLnJomCYyxsB1yMZo-dSCdhPl58YSs@hawk.rmq.cloudamqp.com/lfjntxwl
+
+import pika
+
+
+params = pika.URLParameters('amqps://lfjntxwl:ajLLnJomCYyxsB1yMZo-dSCdhPl58YSs@hawk.rmq.cloudamqp.com/lfjntxwl')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='hello')
--- admin/products/views.py
+++ admin/products/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Product, User
+from .producer import publish
 from .serializers import ProductSerializer
 import random
 
@@ -10,6 +11,7 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):   #/api/products
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        publish()
         return Response(serializer.data)
 
     def create(self, request):  #/api/products
